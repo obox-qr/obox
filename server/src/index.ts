566784166e518fb652aa84accdb7b5b97aed434e
Yaroslav Andriievskyi <yandriievskyi@ascendix.net>
2024-06-02 14:25:47 +0200

    updated dockerconfigs. setup swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,16 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import { setupSwagger } from './swagger';
 
 const app: Express = express();
 
 const port = process.env.PORT || 3000;
 app.use(cors());
 
+setupSwagger(app);
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hosted on Render with Typescript based Express Server");
+  res.send("Hosted on Render with Typescript based Express Server!");
 });
 
 app.listen(port, () => {
