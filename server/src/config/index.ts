09c24947d9219cd27a3fc345f6aca891c023a543
Yaroslav Andriievskyi <yandriievskyi@ascendix.net>
2024-06-24 16:48:35 +0200

    fix dockerfile

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface IConfig {
-  apiUrl: string;
   dbConnectionString: string;
   jwtSecret: string;
   refreshSecret: string;
@@ -12,7 +11,6 @@ interface IConfig {
 }
 
 export const Config: IConfig = {
-  apiUrl: process.env.API_URL!,
   dbConnectionString: process.env.DB_CONNECTION_STRING!,
   jwtSecret: process.env.JWT_SECRET!,
   refreshSecret: process.env.REFRESH_SECRET!,
@@ -20,7 +18,6 @@ export const Config: IConfig = {
   refreshExpiryHours: 24,
 }
 
-console.log('API_URL:', process.env.API_URL);
 console.log('DB_CONNECTION_STRING:', process.env.DB_CONNECTION_STRING);
 console.log('JWT_SECRET:', process.env.JWT_SECRET);
 console.log('REFRESH_SECRET:', process.env.REFRESH_SECRET);
