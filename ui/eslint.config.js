fb9b4560b22fb3bd00ce33b489535884ce6d0676
ptrodion <rdnpetrenko@gmail.com>
2024-06-25 23:12:38 +0100

    added first part of rules for esLint

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -1,15 +1,41 @@
+import js from "@eslint/js";
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
+import eslintReact from "eslint-plugin-react";
+import eslintReact from "eslint-plugin-react";
+import eslintReactHooks from "eslint-plugin-react-hooks";
+import eslintReactRefresh from "eslint-plugin-react-refresh";
+import eslintTypeScript from "typescript-eslint";
+// import { languageOptions } from "eslint-plugin-react/configs/all";
+// import { plugins } from "eslint-plugin-react/configs/all";
 
+// import tseslint from "typescript-eslint";
+// import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+// import { fixupConfigRules } from "@eslint/compat";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+  {
+    plugins: {
+      react: eslintReact,
+      'react-hooks': eslintReactHooks,
+      'react-refresh': eslintReactRefresh,
+    }
+  },
+  {
+    ignores: ['node_modules']
+  },
+ {languageOptions: {
+    globals: {
+      ...globals.node,
+      ...globals.browser,
+      ...globals.es2022,
+
+    }
+  }}, 
+
+  {files: ["**/*.{js,ts,jsx,tsx}"]},
+  // { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  // {languageOptions: { globals: globals.browser }},
+  // pluginJs.configs.recommended,
+  // ...tseslint.configs.recommended,
+  // ...fixupConfigRules(pluginReactConfig),
 ];
