be337bb0114245523d42cf5ea38db2d594c66628
ptrodion <rdnpetrenko@gmail.com>
2024-06-28 21:13:20 +0100

    added config for eSLint 9.5.0 and cnahged mistake in pack.json

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -1,45 +1,62 @@
-import js from "@eslint/js";
-import globals from "globals";
-import eslintReact from "eslint-plugin-react";
-import eslintReact from "eslint-plugin-react";
-import eslintReactHooks from "eslint-plugin-react-hooks";
-import eslintReactRefresh from "eslint-plugin-react-refresh";
-import { ESLint } from "@typescript-eslint/eslint-plugin";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import eslintReact from 'eslint-plugin-react';
+import eslintReactHooks from 'eslint-plugin-react-hooks';
+import eslintReactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-plugin-prettier';
+import airbnbBase from "eslint-config-airbnb-base/rules/all";
+import airbnbHooks from "eslint-config-airbnb/rules/react-hooks";
+import airbnbReact from "eslint-config-airbnb/rules/react";
 
-
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     plugins: {
-      react: eslintReact,
+      '@typescript-eslint': tseslint,
+      'react': eslintReact,
       'react-hooks': eslintReactHooks,
       'react-refresh': eslintReactRefresh,
-      '@typescript-eslint': ESLint,
-    }
+      prettier: prettierPlugin,
+    },
+    overrides: [
+      {
+        files: ['**/*.{js,ts,jsx,tsx}'],
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+          ecmaVersion: 'latest',
+          sourceType: 'module',
+          project: ['tsconfig.json', 'tsconfig.node.json'],
+        },
+        rules: {
+          ...airbnbBase.rules,
+          ...airbnbReact.rules,
+          ...airbnbHooks.rules,
+          ...prettierPlugin.configs.recommended.rules,
+          'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+          'prefer-const': 'error',
+          'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+          'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+          'react/self-closing-comp': ['error', { component: true, html: true }],
+          'max-lines': ['warn', { max: 124 }],
+        },
+      },
+      {
+        files: ['**/*.{js,ts,jsx,tsx}'],
+        ignores: ['dist', 'node_modules', 'eslint.config.js'],
+      },
+      js.configs.recommended,
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
   },
-  {
-    ignores: ['node_modules']
-  },
-  {languageOptions: {
-    globals: {
-      ...globals.node,
-      ...globals.browser,
-      ...globals.es2022,
-    }
-  }}, 
-
-  {files: ["**/*.{js,ts,jsx,tsx}"]},
-
 ];
-
-// import { languageOptions } from "eslint-plugin-react/configs/all";
-// import { plugins } from "eslint-plugin-react/configs/all";
-
-// import tseslint from "typescript-eslint";
-// import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-// import { fixupConfigRules } from "@eslint/compat";
-
-  // { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  // {languageOptions: { globals: globals.browser }},
-  // pluginJs.configs.recommended,
-  // ...tseslint.configs.recommended,
-  // ...fixupConfigRules(pluginReactConfig),
