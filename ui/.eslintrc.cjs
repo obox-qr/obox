c91bdcee0aa4a107ed50bdf55f72e3a5d5395694
ptrodion <rdnpetrenko@gmail.com>
2024-07-09 21:10:15 +0100

    Corrected mistakes and added some rules for last emtpy line

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -27,25 +27,36 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'react', 'react-refresh', 'prettier'],
   rules: {
+    // Disallow the use of console, but allow the use of console.error
     'no-console': [
       'error',
       {
         allow: ['error'],
       },
     ],
+    // Disable the rule that requires React to be in scope when using JSX
     'react/react-in-jsx-scope': 'off',
+    // Disable the warning about missing displayName in React components
     'react/display-name': 'off',
+    // Enforce 2-space indentation
     indent: ['error', 2],
+    // Enforce Unix line endings (LF)
     'linebreak-style': ['error', 'unix'],
+    // Enforce the use of single quotes for strings
     quotes: ['error', 'single'],
+    // Enforce the use of semicolons at the end of statements
     semi: ['error', 'always'],
-    // Правила для определения функциональных компонентов
+    // Rules for defining functional components
     'react/function-component-definition': [
       'error',
       {
+        // Require the use of arrow functions for named components
         namedComponents: 'arrow-function',
+        // Require the use of arrow functions for unnamed components
         unnamedComponents: 'arrow-function',
       },
     ],
+    // Enforce newline at the end of file, with no multiple empty lines
+    'eol-last': ['error', 'always'],
   },
 };
--- ui/src/components/button-to-show-tests-style/button.tsx
+++ ui/src/components/button-to-show-tests-style/button.tsx
@@ -1,5 +1,11 @@
-import './button.scss'
+import './button.scss';
 // this is a fake button to see how the tests work
-export const Button = () => {
-    return <button className="button">Button</button>
-}
+const Button = () => {
+  return (
+    <button className="button" type="button">
+      Button
+    </button>
+  );
+};
+
+export default Button;
