22163cb4c6ffc97c9611f2e9b7d44571b0b93f12
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-08-01 00:37:47 +0300

    create TS interface for theme

---
+++ ui/src/styles/theme.types.ts
@@ -0,0 +1,89 @@
+export interface CustomPalette {
+  custom: {
+    systemInfo: string;
+    systemWarning: string;
+    systemError: string;
+		systemSuccess: string;
+		
+    primaryGreenDarkest: string;
+    primaryGreenDarker: string;
+    primaryGreenDark: string;
+    primaryGreen: string;
+    primaryGreenLight: string;
+    primaryGreenLighter: string;
+    primaryGreenLightest: string;
+    primaryGreenPale: string;
+    primaryGreenVeryPale: string;
+
+    greyGreenDarkest: string;
+    greyGreenDarker: string;
+    greyGreenDark: string;
+    greyGreen: string;
+    greyGreenLight: string;
+    greyGreenLighter: string;
+    greyGreenLightest: string;
+    greyGreenPale: string;
+    greyGreenVeryPale: string;
+
+    black: string;
+    dark: string;
+    greyDark: string;
+    greyLighter: string;
+    greyPale: string;
+    whitePure: string;
+
+    accentOrangeDarkest: string;
+    accentOrangeDarker: string;
+    accentOrangeDark: string;
+    accentOrange: string;
+    accentOrangeLight: string;
+    accentOrangeLighter: string;
+    accentOrangeLightest: string;
+    accentOrangePale: string;
+    accentOrangeVeryPale: string;
+
+    fontFamily: string;
+
+    fontSizeLarge: string;
+    fontSizeLargeMedium: string;
+    fontSizeMedium: string;
+    fontSizeMediumSmall: string;
+    fontSizeSmall: string;
+    fontSizeSmaller: string;
+    fontSizeExtraSmall: string;
+    fontSizeTiny: string;
+
+    lineHeightLoose: number;
+    lineHeightRelaxed: number;
+    lineHeightNormal: number;
+    lineHeightTight: number;
+
+    fontWeightUltraBold: number;
+    fontWeightBold: number;
+    fontWeightMedium: number;
+    fontWeightRegular: number;
+
+    letterSpacing: string;
+
+    borderStyle: string;
+
+    borderWidthQrCode: string;
+    borderWidthTabs: string;
+    borderWidthCommon: string;
+
+    borderRadiusExtraLarge: string;
+    borderRadiusLarge: string;
+    borderRadiusMediumLarge: string;
+    borderRadiusMedium: string;
+    borderRadiusSmallMedium: string;
+    borderRadiusSmall: string;
+    borderRadiusExtraSmall: string;
+
+    selectBtnBoxShadow: string;
+    colorPickerBoxShadow: string;
+    emojiBoxShadow: string;
+    popUpBoxShadow: string;
+    registrationFlowBoxShadow: string;
+    toastMessageBoxShadow: string;
+  };
+}
